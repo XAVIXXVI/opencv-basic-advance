375ad5b007a29342596d04bf1aa0da6ad2c1045f
XAVIXXVI <jeslinxavi7@gmail.com>
2024-03-21 21:57:28 +0530

    OpenCV basic to advance

--- opencv/smoothing.py
+++ opencv/smoothing.py
@@ -1,7 +1,23 @@
 import cv2 as cv
 
-img = cv.imread("opencv/photos/cats.jpg")
+img = cv.imread("photos/cats.jpg")
 cv.imshow("Cats", img)
+
+# Averaging
+average = cv.blur(img, (3, 3))
+cv.imshow("Average Blur", average)
+
+# Gaussian Blur
+gaussian = cv.GaussianBlur(img, (3, 3), 0)
+cv.imshow("Gaussian Blur", gaussian)
+
+# Median Blur
+median = cv.medianBlur(img, 3)
+cv.imshow("Median Blur", median)
+
+# Bilateral Blur
+bilateral = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow("Bilateral", bilateral)
 
 
 cv.waitKey(0)
